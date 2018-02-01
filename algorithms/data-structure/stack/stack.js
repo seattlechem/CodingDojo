d10e35307d009f8fb8b605a5156f5142f1fdd407
Peter Kim <seattlechem@yahoo.com>
2018-02-01 12:55:02 -0800

    data structure

---
+++ algorithms/data-structure/stack/stack.js
@@ -0,0 +1,18 @@
+// stack.js
+var Stack = (function(){
+  function Stack() {
+    this.dataStore = [];
+    this.top = 0;
+  }
+  Stack.prototype.push = function(item) {
+    // your code here
+  }
+  Stack.prototype.pop = function() {
+    // your code here
+  }
+  Stack.prototype.display = function() {
+    // your code here
+  }
+  return Stack;
+})();
+module.exports = Stack;
