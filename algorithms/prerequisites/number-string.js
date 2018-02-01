bf5b1cd77f29dc1fbf39f5ac72be6ef17137dfea
Peter Kim <seattlechem@yahoo.com>
2018-02-01 11:28:44 -0800

    Number to String

---
+++ algorithms/prerequisites/number-string.js
@@ -0,0 +1,14 @@
+// Assignment: Number to String
+// Write a program that takes an array of numbers and replaces any number that's negative to a string called 'Dojo'. For example if array = [-1, -3, 2] after your program is done array should be ['Dojo', 'Dojo', 2]
+
+var arr = [-1, -3, 2];
+function numToString(arr){
+  for(var i in arr){
+    if(arr[i] < 0){
+      arr[i] = 'Dojo';
+    }
+  }
+  return arr;
+}
+
+numToString(arr);
