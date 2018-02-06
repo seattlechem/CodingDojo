7c05b58066b2e3270830b68c182324b5bd98cabc
Peter Kim <seattlechem@yahoo.com>
2018-02-05 21:10:10 -0800

    rBC.js

---
+++ algorithms/byte-of-computer-science/rBS.js
@@ -0,0 +1,20 @@
+// Write a function rBS that behaves like the following:
+
+// var arr = [-90,-19,0,2,12,29,33,190,320];
+// rBS(arr, 5)              => false
+// rBS(arr, 12)             => 4
+// rBS(arr, 0)              => 2
+// rBS(arr, 190)            => 7
+// rBS takes in an array and a value to search for. If the value is found within the array then rBS will return the index where the found value is. If the value is not found within the array then rBS returns false. Make sure your solution is recursive. Does your function need to have optional additional parameters? After that first initial call, successive recursive calls to itself might need these
+
+var arr = [-90,-19,0,2,12,29,33,190,320];
+
+function iBS(arr, num){
+  if{
+    for(var i = 0; i < arr.length; i++){
+      
+    }else{
+      return false;
+    }
+  }
+}
